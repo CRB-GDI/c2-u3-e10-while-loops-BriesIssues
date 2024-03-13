560e2235b272f4216f2461b7a0c4f6f263df07c3
Brianna Rodriguez <145356670+BriesIssues@users.noreply.github.com>
2024-03-13 10:16:30 -0400

    not done

---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1 - New Game ");
+            Console.WriteLine("2 - Load Game ");
+            Console.WriteLine("3 - Options ");
+            Console.WriteLine("4 - Quit ");
+            Console.WriteLine("**************");
+            int gameMenu = int.Parse(Console.ReadLine());
+
+            if (gameMenu == 1)
+            {
+                Console.WriteLine("Starting new game ");
+            }
+            if (gameMenu == 2)
+            {
+                Console.WriteLine("Loading game ");
+            }
+            if (gameMenu == 3)
+            {
+                Console.WriteLine("Audio");
+                Console.WriteLine("Hud");
+                Console.WriteLine("Controles");
+            }
+            if (gameMenu == 4)
+            {
+              // quitting the game  
+            }
+        }
+    }
+}
