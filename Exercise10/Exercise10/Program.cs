9b3c4f37fe5452dd387e22a6c13fefe9403543af
Brianna Rodriguez <145356670+BriesIssues@users.noreply.github.com>
2024-03-14 09:41:12 -0400

    finished Problem 1

--- Exercise10/Exercise10/Program.cs
+++ Exercise10/Exercise10/Program.cs
@@ -13,24 +13,35 @@
             Console.WriteLine("**************");
             int gameMenu = int.Parse(Console.ReadLine());
 
-            if (gameMenu == 1)
+            while (gameMenu >= 1 || gameMenu <= 4)
             {
-                Console.WriteLine("Starting new game ");
-            }
-            if (gameMenu == 2)
-            {
-                Console.WriteLine("Loading game ");
-            }
-            if (gameMenu == 3)
-            {
-                Console.WriteLine("Audio");
-                Console.WriteLine("Hud");
-                Console.WriteLine("Controles");
-            }
-            if (gameMenu == 4)
-            {
-              // quitting the game  
-            }
+                if (gameMenu == 1)
+                {
+                    Console.WriteLine("Starting new game ");
+                }
+                if (gameMenu == 2)
+                {
+                    Console.WriteLine("Loading game ");
+                }
+                if (gameMenu == 3)
+                {
+                    Console.WriteLine("Audio");
+                    Console.WriteLine("Hud");
+                    Console.WriteLine("Controles");
+                }
+                if (gameMenu == 4)
+                {
+                    // quitting the game  
+                }
+                Console.WriteLine("**************");
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1 - New Game ");
+                Console.WriteLine("2 - Load Game ");
+                Console.WriteLine("3 - Options ");
+                Console.WriteLine("4 - Quit ");
+                Console.WriteLine("**************");
+                int gameMenu2 = int.Parse(Console.ReadLine());
+            } // end of while
         }
     }
 }
